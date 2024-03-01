5b8467eb296fe2434e2afff696ad04325e34edfb
Frank <kamunyuz@gmail.com>
2024-03-01 10:41:18 +0300

    subs reddit

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,25 +1,17 @@
 #!/usr/bin/python3
-"""
-Script that queries the Reddit API and returns the number of subscribers
-for a given subreddit.
-"""
-import requests
+"""Module for task 0"""
 
 
 def number_of_subscribers(subreddit):
-    """
-        return number of subscribers for a given subreddit
-        return 0 if invalid subreddit given
-    """
-    url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
+    """Queries the Reddit API and returns the number of subscribers
+    to the subreddit"""
+    import requests
 
-    # get user agent
-    # https://stackoverflow.com/questions/10606133/ -->
-    headers = requests.utils.default_headers()
-    headers.update({'User-Agent': 'My User Agent 1.0'})
-
-    r = requests.get(url, headers=headers).json()
-    subscribers = r.get('data', {}).get('subscribers')
-    if not subscribers:
+    sub_info = requests.get("https://www.reddit.com/r/{}/about.json"
+                            .format(subreddit),
+                            headers={"User-Agent": "My-User-Agent"},
+                            allow_redirects=False)
+    if sub_info.status_code >= 300:
         return 0
-    return subscribers
+
+    return sub_info.json().get("data").get("subscribers")
